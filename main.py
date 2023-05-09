a6aeadafee2e1a4e2b72e9fbcbea59466f9ce5ab
kristinical <ebermank@oregonstate.edu>
2023-05-09 13:57:46 -0700

    Updates to solo study

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from questions import questions
 import random
 
@@ -15,9 +15,9 @@ def index():
     return render_template('index.html')
 
 # This route takes the user to the solo study section of the website
-@app.route('/solo', )
+@app.route('/quiz', )
 def solo_study():
-    #Setting the appropriate values for keeping track of the score
+    # Setting the appropriate values for keeping track of the score
     asked_q.clear()
     score.clear()
     score.append(0)
@@ -30,12 +30,7 @@ def solo_study():
 # a ton to make it work with our program
 @app.route('/test', methods=['GET'])
 def test():
-    if request.method == 'GET':
-        # Check if user is at the end of the questions
-        if len(asked_q) == len(questions):
-            asked_q.clear()
-            return render_template('test_end.html', correct = get_score())
-        
+    if request.method == 'GET':        
         # Iterate through the questions to get the last
         q_idx = 0
         question_obj = questions[q_idx]
@@ -49,18 +44,23 @@ def test():
         asked_q.append(question)
         choices = question_obj["choices"]
         answer = question_obj["answer"]
-        choices = question_obj["choices"]
-        answer = question_obj["answer"]
+        hints = question_obj["hints"]
 
-
-        return(render_template('test.html', question = question, choices = choices, answer = answer))
+        return(render_template('test.html', question = question, choices = choices, answer = answer, hints = hints))
     
 # This route shows the user a page showing their results for the previous question
 @app.route('/result', methods=['POST'])
 def results():
+    # Check if user is at the end of the questions
+    end = False
+    if len(asked_q) == len(questions):
+        end = True
+
     # Gets the users response https://stackoverflow.com/questions/31662681/flask-handle-form-with-radio-buttons 
     user_ans = request.form.get('options')
     user_ans = user_ans[0]
+    choices = []
+    hints = []
 
     # Gets the last question that was asked
     question = asked_q[len(asked_q)-1]
@@ -70,17 +70,36 @@ def results():
         question_obj = questions[i]
         if question_obj['question'] == question:
             real_ans = question_obj['answer']
+            for choice in question_obj['choices']:
+                choices.append(choice)
+            for hint in question_obj['hints']:
+                hints.append(hint)
             break
     
     # If the user got the correct answer, increment their score by one
     if user_ans == real_ans:
+        result = "correct!"
         score.append(get_score() + 1)
-    return render_template('results.html', correct = get_score())
+    else:
+        result = "incorrect"
+    
+    # So that full word is printed on the results page
+    if user_ans == "T":
+        user_ans = "True"
+    elif user_ans == "F":
+        user_ans = "False"
+
+    return render_template('results.html', correct = get_score(), total_qs = len(questions), qs_asked = len(asked_q), 
+                           percent = get_percentage(), real_answer = real_ans, result = result, question = question, 
+                           choices = choices, answer = user_ans, hints = hints, end = end)
+
+@app.route('/quiz_end', methods=['GET'])
+def quiz_end():
+    return(render_template('quiz_end.html', correct = get_score(), total_qs = len(asked_q), percent = get_percentage()))
 
 # This route takes the user to the class game page 
 @app.route('/game')
 def class_game():
-
     return render_template('game.html')
 
 # This route takes the user to the admin login page
@@ -95,6 +114,12 @@ def admin():
 def get_score():
     return score[len(score)-1]
 
+def get_percentage():
+    if len(asked_q) != 0:
+        percent = get_score() / int(len(asked_q)) * 100
+        return int(percent)
+    else:
+        return 0
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
--- questions.py
+++ questions.py
@@ -10,33 +10,39 @@ questions = [
     {
         "question": "Reduced ice leads to greater absorption of solar radiation.",
         "choices": ["True", "False"],
-        "answer": "True"
+        "answer": "T",
+        "hints": ["ice_melt.jpg"]
     },
     {
         "question": "The lapse rate helps explain the role of latitude in impacting climate.",
         "choices": ["True", "False"],
-        "answer": "False"
+        "answer": "F",
+        "hints": ["lapse_rate.png"]
     },
     {
         "question": "Greenhouse gases absorb visible light from the Sun.",
         "choices": ["True", "False"],
-        "answer": "False"
+        "answer": "F",
+        "hints": []
     },
     {
         "question": "Greenhouse gases absorb and re-radiate infrared radiation (heat energy).",
         "choices": ["True", "False"],
-        "answer": "True"
+        "answer": "T",
+        "hints": []
     },
     {
         "question": "There has been more warming in the day than at night.",
         "choices": ["True", "False"],
-        "answer": "False"
+        "answer": "F",
+        "hints": []
     },
     {
-        "question": "Greenhouse gasses impact:",
+        "question": "Greenhouse gases impact:",
         "choices": ["A. incoming solar radiation", "B. loss of infrared radiation",
                     "C. Both A and B", "D. None of the above"],
-        "answer": "B"
+        "answer": "B",
+        "hints": ["greenhouse_effects.jpg"]
     },
     {
         "question": "Compared to the amount of radiation received on the surface"
@@ -44,7 +50,8 @@ questions = [
                     " surface of the Earth receive reradiated back from greenhouse gases?",
         "choices": ["A. About 1/2 as much", "B. About 1/4 as much",
                     "C. An equal amount", "D. About twice as much"],
-        "answer": "D"
+        "answer": "D",
+        "hints": ["radiation.png"]
     },
     {
         "question": "When does climate change occur?",
@@ -52,37 +59,49 @@ questions = [
                     "B. When in coming and out going radiation are not in balance",
                     "C. When there is consistently no incoming radiation",
                     "D. When there is consistently no outgoing radiation"],
-        "answer": "B"
+        "answer": "B",
+        "hints": ["radiation.png"]
+    },
+    {
+        "question": "Plants use CO2 and H2O to produce glucose (sugar) necessary to survive in which process?",
+        "choices": ["A. Electrophoresis", "B. Photosynthesis", "C. Carbon fusion", "D. Bioengineering", "E. Respiration"],
+        "answer": "B",
+        "hints": ["plants.jpg"]
     },
     {
         "question": "Which process led to the removal of CO2 from the atmosphere in the Carboniferous period?",
-        "choices": ["A. Chemosynthesis", "B. Respiration", "C. Carbon fusion", "D. Photosynthesis"],
-        "answer": "D"
+        "choices": ["A. Chemosynthesis", "B. Respiration", "C. Carbon fusion", "D. Photosynthesis", "E. Respiration"],
+        "answer": "D",
+        "hints": ["carboniferous.jpg"]
     },
     {
         "question": "Latent heat is involved in what processes?",
         "choices": ["A. Phase changes between ice, liquid water, and water vapor",
                     "B. Temperature changes", "C. Both A and B", "D. None of the above"],
-        "answer": "A"
+        "answer": "A",
+        "hints": ["latent_heat.jpg", "radiation.png"]
     },
     {
         "question": "Which of the following is a measure of the motion of particles contained in a substance?",
         "choices": ["A. Temperature", "B. Heat Energy", "C. Heat Flow",
                     "D. None of the above", "E. All of the above"],
-        "answer": "A"
+        "answer": "A",
+        "hints": ["particles.png"]
     },
     {
         "question": "Ocean temperatures are monitored by:",
         "choices": ["A. >3000 ARGO floats", "B. The ENSO monitoring system in the Pacific ocean",
                     "C. Satellites", "D. All of the above"],
-        "answer": "D"
+        "answer": "D",
+        "hints": ["ocean_temp.jpg", "enso.jpg"]
     },
     {
         "question": "What are common characteristics of El Nino and La Nina?",
         "choices": ["A. Changes in the tropical Pacific sea surface temperature",
                     "B. Significant shifts in climate throughout the globe",
                     "C. Both A and B", "D. None of the above"],
-        "answer": "C"
+        "answer": "C",
+        "hints": ["el_nino.jpg", "la_nina.jpg", "el_nino_year.jpg"]
     },
     {
         "question": "How do the conditions during La Nina phase of ENSO compare to those in the El Nino phase?",
@@ -90,14 +109,16 @@ questions = [
                     "C. La Nina stores heat in the ocean while El Nino releases heat to the atmosphere",
                     "D. El Nino stores heat in the ocean while La Nina releases heat to the atmosphere",
                     "E. B and C are correct"],
-        "answer": "E"
+        "answer": "E",
+        "hints": []
     },
     {
         "question": "When compared to Gyres in the Northern hemisphere, "
                     "what is the direction of gyres in the southern hemisphere?",
         "choices": ["A. Opposite", "B. The same", "C. Depends on the season",
                     "D. Depends on salinity", "E. A and D"],
-        "answer": "A"
+        "answer": "A",
+        "hints": ["gyres.jpg"]
     },
     {
         "question": "Which of the following is true about the Oregon climate?",
@@ -106,7 +127,8 @@ questions = [
                     "B. It rains very little in the summer because the land is generally warmer than the ocean",
                     "C. The coast is foggy in the summer because upwelling brings cold water to the surface",
                     "D. B and C", "E. A, B and C"],
-        "answer": "D"
+        "answer": "D",
+        "hints": ["oregon.png", "westcoast.jpg"]
     },
     {
         "question": "Which do we know about precipitation?",
@@ -114,20 +136,23 @@ questions = [
                     "B. It rains more once the air has crossed a mountain pass",
                     "C. Hadley cells create more rain at the equator but deserts at 30 degrees N and S latitudes",
                     "D. A and C", "E. A, B and C"],
-        "answer": "C"
+        "answer": "C",
+        "hints": []
     },
     {
         "question": "In what ways is heat circulated from the warm ocean to the cold poles?",
         "choices": ["A. Through Hadley cells and tornados", "B. Through the stratospheric O3 hole",
                     "C. Through ocean gyres and the great ocean conveyor belt", "D. A and C", "E. A, B and C"],
-        "answer": "D"
+        "answer": "D",
+        "hints": []
     },
     {
         "question": "CO2 is higher now than:",
         "choices": ["A. The past 100 years",
                     "B. The past 10,000 years while humans have had agrarian life styles dependent on stable crops",
                     "C. The past 60,000 years", "D. The past 600,000 years during the coming and going of ice ages"],
-        "answer": "D"
+        "answer": "D",
+        "hints": ["co2_levels.png"]
     },
     {
         "question": "What proportion of our CO2 emissions have ended up in the air compared to the oceans and land plants?",
@@ -136,7 +161,8 @@ questions = [
                     "C. 75% in the air, 25% in the oceans, and none in land plants",
                     "D. 0% in the air or land plants – all the CO2 humans have emitted has been absorbed "
                     "by the oceans and is causing ocean acidification"],
-        "answer": "D"
+        "answer": "D",
+        "hints": ["co2_emissions.png"]
     },
     {
         "question": "Which of the following is true about greenhouse gases?",
@@ -145,7 +171,8 @@ questions = [
                     "B. Humans currently emit greenhouse gases but do not take them h=out of the atmosphere",
                     "C. Natural ecosystems currently emit even more greenhouse gases than humans, "
                     "but also take them out of the atmosphere", "D. A and B", "E. A, B and C"],
-        "answer": "E"
+        "answer": "E",
+        "hints": ["greenhouse_gases.png"]
     },
     {
         "question": "How do we know whether the higher CO2 concentration were caused by humans?",
@@ -153,7 +180,8 @@ questions = [
                     "B. Tree rings show a decrease in 14C signatures reflective of the addition of the fossil fuel carbon to the atmosphere",
                     "C. Ice cores show a decrease in 13C signatures reflective of the addition of fossil fuel carbon to the atmosphere",
                     "D. A and B", "E. A, B and C"],
-        "answer": "E"
+        "answer": "E",
+        "hints": ["tree_rings.png", "ice_core.png", "ice_core2.png"]
     },
     {
         "question": "How does water vapor act to amplify effect of CO2 on warming the climate system without initiating it?",
@@ -161,18 +189,21 @@ questions = [
                     "B. Water vapor rains out of the atmosphere within weeks of when it cools so has no long lasting impact",
                     "C. Water vapor follows the temperature changes rather than initiating them",
                     "D. All of the above"],
-        "answer": "D"
+        "answer": "D",
+        "hints": []
     },
     {
         "question": "Which of the following can account for increased heat in the "
-                    "Earth’s climate system that does not alter the air temperature?",
+                    "Earth's climate system that does not alter the air temperature?",
         "choices": ["A. Latent heat of vaporization", "B. Latent heat of ice melting",
                     "C. Absorption of heat energy into the ocean", "D. A and B", "E. A, B and C"],
-        "answer": "D"
+        "answer": "D",
+        "hints": ["ocean_heat.jpg"]
     },
     {
         "question": "Which provides more radiation to the Surface of the Earth?",
         "choices": ["A. The Sun", "B. Greenhouse Gases", "C. They are equal"],
-        "answer": "B"
+        "answer": "B",
+        "hints": []
     },
 ]
